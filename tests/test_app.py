8fd64c6684dbb92f9a2b4fe2fd63185d868e5baa
Kristofer Fangrat <kristofer.fangrat@gmail.com>
2024-11-16 11:50:54 +0100

    changes made

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
-# test_app.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from streamlit_app import layout
 from get_weather import WeatherData
 import unittest
