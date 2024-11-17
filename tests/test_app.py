7b75eed59ed86e8f9f3e19411d41f355fd306254
Kristofer Fangrat <kristofer.fangrat@gmail.com>
2024-11-17 13:22:15 +0100

    fixing error within streamlit_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,7 @@
-# test_app.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import unittest
 from unittest.mock import patch, MagicMock
 from streamlit_app import layout
