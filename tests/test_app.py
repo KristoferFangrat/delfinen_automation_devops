a45557f3f06cc172881e0c5827a03f0e81e59fe4
Kristofer Fangrat <kristofer.fangrat@gmail.com>
2024-11-19 14:50:59 +0100

    fixing error

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 import unittest
 from unittest.mock import patch, MagicMock
-from streamlit_app import layout
+from github.AutomationDevOps.DevOpsProject.delfinen_automation_devops.app.streamlit_app import layout
 from get_weather import WeatherData
 
 class TestStreamlitApp(unittest.TestCase):
